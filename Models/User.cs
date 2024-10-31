8687c9122136f104d9c16da2d40c63af82b1b4cc
Connor Zittrauer <zittrauerconnor@gmail.com>
2024-10-31 10:08:26 -0500

    added user class

--- Models/User.cs
+++ Models/User.cs
@@ -9,5 +9,14 @@ namespace Trackit.Models
 
     public class User
     {
+        public int UserID { get; private set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public User(string username, string password) 
+        { 
+            this.UserName = username;
+            this.Password = password;
+        }
     }
 }
