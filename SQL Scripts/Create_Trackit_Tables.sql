8107178c0281144f7a666907b618d6a0afa532eb
Connor Zittrauer <zittrauerconnor@gmail.com>
2024-11-01 14:36:08 -0500

    merged master SQL script

--- SQL Scripts/Create_Trackit_Tables.sql
+++ SQL Scripts/Create_Trackit_Tables.sql
@@ -1,3 +1,5 @@
+DROP TABLE Task, AppUser;
+
 CREATE TABLE AppUser (
 	UserID INT PRIMARY KEY IDENTITY(1,1),    -- Auto increment primary key by 1, starting at 1
 	Username NVARCHAR(50) NOT NULL UNIQUE, 
@@ -5,17 +7,15 @@ CREATE TABLE AppUser (
 );
 
 CREATE TABLE Task (
-    TaskID INT PRIMARY KEY IDENTITY(1,1),    -- Auto increment primary key by 1, starting at 1
-	Username NVARCHAR(50) NOT NULL, 
-    UserID INT NOT NULL,                     -- Foreign key to AppUser table
-    TaskName NVARCHAR(100) NOT NULL,         
-    TaskDescription NVARCHAR(255),           
-    DueDate DATETIME,                        
-    DateCreated DATETIME DEFAULT GETDATE(),  
-	DateCompleted DATETIME, 
-    IsCompleted BIT DEFAULT 0,               
+    TaskID INT PRIMARY KEY IDENTITY(1,1),
+    UserID INT NOT NULL,
+    TaskName NVARCHAR(100) NOT NULL,
+    TaskDescription NVARCHAR(255),
+    DueDate DATETIME,
+    DateCreated DATETIME DEFAULT GETDATE(),
+    DateCompleted DATETIME,
+    IsCompleted BIT DEFAULT 0,
 
-    -- Define the foreign key constraint to link Task to User
     CONSTRAINT FK_Task_User FOREIGN KEY (UserID) REFERENCES AppUser(UserID)
-    ON DELETE CASCADE                        -- Optional: delete tasks if user is deleted
+    ON DELETE CASCADE
 );
